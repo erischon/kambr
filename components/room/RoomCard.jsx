76fe380e63bc9828504b2eee8f39c4dfd4dd635e
Eri Schon <erischon@gmail.com>
2022-05-13 14:31:18 +0200

    Update RoomCard.jsx

--- components/room/RoomCard.jsx
+++ components/room/RoomCard.jsx
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { BsInfoSquare } from "react-icons/bs";
 
 import { urlFor } from "../../lib/client";
 import styles from "../../styles/room.module.scss";
@@ -7,14 +9,27 @@ import styles from "../../styles/room.module.scss";
 const RoomCard = ({ room: { image, name } }) => {
   return (
     <Fragment>
-      <div className={styles.roomCardContainer}></div>
-      <Image
-        src={urlFor(image && image[0]).url()}
-        width={200}
-        height={200}
-        alt={name}
-      />
-      <div>{name}</div>
+      <div className={styles.roomCardContainer}>
+        <div className={styles.roomCardImg}>
+          <Image
+            src={urlFor(image && image[0]).url()}
+            layout="fill"
+            objectFit="cover"
+            alt={name}
+            quality={75}
+            priority={true}
+          />
+        </div>
+        <div className={styles.roomCardBottom}>
+          <h2 className={styles.roomCardTitle}>{name}</h2>
+          <Link href="/">
+            <a className={styles.roomCardInfo}>
+              <BsInfoSquare />
+            </a>
+          </Link>
+        </div>
+        <button className={styles.roomCardButton}>Réserver</button>
+      </div>
     </Fragment>
   );
 };
