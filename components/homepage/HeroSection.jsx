76c80df9ad20cc44c7f23f99aabce58a88c8e3d2
Eri Schon <erischon@gmail.com>
2022-05-13 08:48:58 +0200

    Update HeroSection.jsx

--- components/homepage/HeroSection.jsx
+++ components/homepage/HeroSection.jsx
@@ -6,12 +6,18 @@ const HeroSection = () => {
   return (
     <Fragment>
       <section className={styles.heroContainer}>
-        <h1 className={styles.heroTitle}>La ferme de Kerouac</h1>
-        <p className={styles.heroSubtitle}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
-          enim, illo tempore vitae molestias expedita tempora fugit dolor!{" "}
-        </p>
-        <button className={styles.heroBtn}>Réserver</button>
+        <div className={styles.heroBox}>
+          <div className={styles.heroLeft}>
+            <h1 className={styles.heroTitle}>La ferme de Kerouac</h1>
+            <p className={styles.heroSubtitle}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+              enim, illo tempore vitae molestias expedita tempora fugit dolor!{" "}
+            </p>
+            <button className={styles.heroBtn}>Réserver</button>
+          </div>
+
+          <div className={styles.heroRight}></div>
+        </div>
       </section>
     </Fragment>
   );
