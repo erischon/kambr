0eae84f40d74f14cb6da21be49f125a5ecdeb133
Eri Schon <erischon@gmail.com>
2022-05-14 17:20:33 +0200

    Update RoomDetail.jsx

--- components/room/RoomDetail.jsx
+++ components/room/RoomDetail.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import Image from "next/image";
+import { PortableText } from "@portabletext/react";
 
 import { urlFor } from "../../lib/client";
 import styles from "../../styles/room.module.scss";
 
 const RoomDetail = ({ room }) => {
-  const { name, image, description, price } = room;
+  const { name, image, description, price, infos } = room;
 
   return (
     <Fragment>
@@ -21,6 +22,7 @@ const RoomDetail = ({ room }) => {
         <h1>{name}</h1>
         <p>{description}</p>
         <p>{price}€</p>
+        <PortableText value={infos} />
       </div>
     </Fragment>
   );
