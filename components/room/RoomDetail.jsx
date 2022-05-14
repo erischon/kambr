ee8a7f167c0fb004dc09ea275bc3a5a62973e386
Eri Schon <erischon@gmail.com>
2022-05-14 16:30:51 +0200

    Update RoomDetail.jsx

--- components/room/RoomDetail.jsx
+++ components/room/RoomDetail.jsx
@@ -1,5 +1,29 @@
-const RoomDetail = () => {
-  return <div>RoomDetail</div>;
+import { Fragment } from "react";
+import Image from "next/image";
+
+import { urlFor } from "../../lib/client";
+import styles from "../../styles/room.module.scss";
+
+const RoomDetail = ({ room }) => {
+  const { name, image, description, price } = room;
+
+  return (
+    <Fragment>
+      <div className={styles.roomDetailContainer}>
+        <div className={styles.roomDetailImg}>
+          <Image
+            src={urlFor(image && image[0]).url()}
+            layout="fill"
+            objectFit="cover"
+            alt={name}
+          />
+        </div>
+        <h1>{name}</h1>
+        <p>{description}</p>
+        <p>{price}€</p>
+      </div>
+    </Fragment>
+  );
 };
 
 export default RoomDetail;
