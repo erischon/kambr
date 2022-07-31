fc252bd2f2e63451e672be9dc81217cec80b33fe
Eri Schon <erischon@gmail.com>
2022-07-31 18:24:18 +0200

    Update seeder.js

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,16 +1,10 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const Room = require("../models/room");
 const rooms = require("../data/rooms.data");
 
-const dbConnect = require("../config/dbConnect");
-dbConnect();
-
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.MONGO_URI);
 
 const seedRooms = async () => {
   try {
