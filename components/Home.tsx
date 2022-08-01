1660b54d8bb62094a69040d51b3893b04a597b48
Eri Schon <erischon@gmail.com>
2022-08-01 15:59:54 +0200

    Update Home.tsx

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/dist/client/image";
+import Hero from "./Hero.component";
 
 const Home = () => {
   return (
     <main className="flex-[6]">
-      <section>hero</section>
+      <Hero />
       <section>Les chambres</section>
       <section>Chez nous on vous propose</section>
       <section>Autour</section>
