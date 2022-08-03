32ac1b916b2e47d4eeeb1729ceb7863c151b14a9
Eri Schon <erischon@gmail.com>
2022-08-03 12:51:35 +0200

    Update Button.component.tsx

--- components/Button.component.tsx
+++ components/Button.component.tsx
@@ -1,21 +1,21 @@
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 const Button = (props: any) => {
   let buttonClass =
     "py-2 px-5 bg-amber-500 rounded-lg items-center gap-1 cursor-pointer inline-flex";
-  let iconClass = "text-xl";
+  let iconClass = "text-lg pt-1";
 
   if (props.size === "lg") {
     buttonClass =
       "py-4 px-8 bg-amber-500 rounded-lg items-center gap-2 cursor-pointer inline-flex text-lg";
-    iconClass = "text-2xl";
+    iconClass = "text-2xl pt-1";
   }
 
   return (
     <div className={buttonClass}>
       <span className="">{props.text}</span>
       <span className={iconClass}>
-        <MdOutlineKeyboardArrowRight />
+        <HiOutlineArrowNarrowRight />
       </span>
     </div>
   );
