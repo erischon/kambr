b9dc302cc1c589697fd9cf5fde76f75c3afa99c4
Eri Schon <erischon@gmail.com>
2022-08-03 11:43:22 +0200

    trying to center icon...

--- components/More.component.tsx
+++ components/More.component.tsx
@@ -1,11 +1,11 @@
-import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 const More = (props: any) => {
   return (
     <div className="flex justify-between items-center w-fit gap-2 mt-8">
       <button className="cursor-pointer">{props.text}</button>
       <span>
-        <ArrowRightAltIcon />
+        <HiOutlineArrowNarrowRight />
       </span>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       backgroundImage: {
         "hero-pattern":
           "linear-gradient(to right, rgba(224, 235, 237, 1), rgba(255, 255, 255, .2)), url('https://images.unsplash.com/photo-1559131463-f9386f12e2db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80')",
+        "icon-arrow-right": "url('../assets/icons/arrow-right.svg')",
       },
     },
   },
