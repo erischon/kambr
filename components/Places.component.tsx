31df9de46385f3913c4a1f71756530f07d9cf0f1
Eri Schon <erischon@gmail.com>
2022-08-04 16:53:02 +0200

    Update Places.component.tsx

--- components/Places.component.tsx
+++ components/Places.component.tsx
@@ -4,7 +4,7 @@ import More from "./More.component";
 const Around = ({ places }: any) => {
   return (
     <section className="my-8 mx-4" id="environs">
-      <h2 className="text-2xl font-bold">Around</h2>
+      <h2 className="text-2xl font-bold">Aux alentours</h2>
 
       <div className="flex flex-row gap-4 flex-wrap justify-evenly mt-8">
         {places.map((place: any) => (
@@ -12,7 +12,7 @@ const Around = ({ places }: any) => {
         ))}
       </div>
 
-      <More text="Autres lieux dans les environs" url="#" />
+      <More text="Autres lieux dans les environs" url="#environs" />
     </section>
   );
 };
