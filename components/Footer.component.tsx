4b337614f825aac2b26a4a04ce516bb50ba26290
Eri Schon <erischon@gmail.com>
2022-08-07 08:59:35 +0200

    Update Footer.component.tsx

--- components/Footer.component.tsx
+++ components/Footer.component.tsx
@@ -4,7 +4,7 @@ import Location from "./Location.component";
 const FooterItems = () => {
   return (
     <div className="grid md:grid-cols-12 mt-8 mb-2 gap-2">
-      <div className="md:col-span-2 bg-gray-700 p-4 rounded-md text-xs">
+      <div className="md:col-span-3 bg-gray-700 p-4 rounded-md text-xs">
         <ul className="flex gap-2 justify-between sm:justify-around md:flex-col flex-wrap">
           <li>A propos</li>
           <li>Mentions légales</li>
@@ -12,7 +12,7 @@ const FooterItems = () => {
         </ul>
       </div>
 
-      <div className="md:col-span-4 bg-gray-700 p-4 rounded-md text-xs">
+      <div className="md:col-span-3 bg-gray-700 p-4 rounded-md text-xs">
         <Contact />
       </div>
 
